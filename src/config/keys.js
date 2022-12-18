98ae3db8813a81015828d6860d67cc76d72d7306
Phyo Kyaw <phyokyawdev@gmail.com>
2022-12-18 13:42:35 +0630

    updated command line options

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,27 +1,38 @@
 const util = require('@ethereumjs/util');
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
+const { program } = require('commander');
 
 /**
  * CHECK CLIENT OPTIONS
  */
-let { port = 3000, nodeType, mainIpAddress, mainAccountAddress } = argv;
 
-if (!nodeType) throw new Error('--nodeType is required');
+program
+  .option('-p, --port <number>', 'port number', 3000)
+  .requiredOption('-t, --node-type <string>', 'node type - "main" | "side"')
+  .requiredOption(
+    '-a, --main-account-address <string>',
+    'address of main account(hex string)'
+  )
+  .option('-i, --main-ip-address <string>', 'ip address of main account');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+let { port, nodeType, mainIpAddress, mainAccountAddress } = options;
+
+if (!nodeType) throw new Error('--node-type is required');
 
 const lower_type = nodeType.toLowerCase();
 
 if (lower_type !== 'main' && lower_type !== 'side')
-  throw new Error('--nodeType accept main or side as argument');
+  throw new Error('--node-type accept main or side as argument');
 
 if (lower_type === 'main' && !mainAccountAddress)
-  throw new Error('--mainAccountAddress is required for --nodeType main');
+  throw new Error('--main-account-address is required for --node-type main');
 
 if (lower_type === 'side' && !mainIpAddress)
-  throw new Error('--mainIpAddress is required for --nodeType side');
+  throw new Error('--main-ip-address is required for --node-type side');
 
-//strip hex prefix for mainAccountAddress
 mainAccountAddress = util.stripHexPrefix(mainAccountAddress);
 
 module.exports = {
